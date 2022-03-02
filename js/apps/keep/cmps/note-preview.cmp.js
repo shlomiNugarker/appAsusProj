3d3d82e9d0fd629cc2407200e7c256ddb8d25804
TalL <tallevi008@gmail.com>
2022-03-02 23:51:16 +0200

    some changes to adding notes

--- js/apps/keep/cmps/note-preview.cmp.js
+++ js/apps/keep/cmps/note-preview.cmp.js
@@ -2,8 +2,11 @@ export default {
     props: ['note'],
     template: `
         <article class="note">
-           <p>{{note.info}}</p> 
-        </article>
+           <p>note.info</p> 
+        <div class="actions">
+            <button>X</button>
+            </div>
+            </article>
     `,
     components: {},
     created() { },
